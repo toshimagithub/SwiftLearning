3130c781b34e0e809395ae4474402b4f1d989e19
toshimagihub <141797646+toshimagithub@users.noreply.github.com>
2024-08-13 11:07:00 +0900

    解説3

--- 7_Clima-iOS13 /Clima/Controller/WeatherViewController.swift
+++ 7_Clima-iOS13 /Clima/Controller/WeatherViewController.swift
@@ -6,138 +6,128 @@
 //  Copyright © 2019 App Brewery. All rights reserved.
 //
 
-import UIKit
-import CoreLocation
+import UIKit //UIViewControllerを使うためにimportしてる
+import CoreLocation //位置情報を取得するフレームワーク
 
 
-// WeatherViewControllerは、天気情報を表示するためのUIViewControllerサブクラスです。
+//UIViewControllerを継承したWeatherViewControllerというクラス
+//基本的にUIViewControllerは継承して使う、iOS のフレームワークの一部であり、UIKit フレームワークに含まれる
 class WeatherViewController: UIViewController {
-
-    // UI部品のアウトレット
-    // WeatherDataManagerインスタンスを作成して、天気情報の取得を管理
-    @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
-    @IBOutlet weak var searchField: UITextField!
+    //IBがInterface Builderの略
+    @IBOutlet weak var conditionImageView: UIImageView!   // 表示画面に天気アイコン（画像）を表示するため
+    @IBOutlet weak var temperatureLabel: UILabel! // 表示画面に温度を表示するため
+    @IBOutlet weak var cityLabel: UILabel!        // 表示画面に都市名を表示するため
+    @IBOutlet weak var searchField: UITextField!   // ユーザーが都市名を入力するためのテキストフィールド
     
     
     //MARK: Properties
-    var weatherManager = WeatherDataManager()
-    let locationManager = CLLocationManager()
+    //型推論を使用して変数にインスタンスを代入
+    var weatherManager = WeatherDataManager() //インスタンスを作成し、天気データの取得や管理を行います
+    let locationManager = CLLocationManager() //インスタンスを作成し、ユーザーの現在位置を取得・管理 import CoreLocationしているからCLLocationManager()が使える
     
+    //UIViewController クラスの viewDidLoad メソッドをオーバーライドしている
     override func viewDidLoad() {
         super.viewDidLoad()
+        //親クラス（UIViewController）の viewDidLoad メソッドを呼び出しています。これにより、親クラスが行うべき標準的な初期化処理が行われます。
         
-        // 各デリゲートの設定
-        locationManager.delegate = self
-        weatherManager.delegate = self
-        searchField.delegate = self
+        locationManager.delegate = self // CLLocationManagerが位置情報のイベントをWeatherViewControllerに通知するよう設定する
+        weatherManager.delegate = self // WeatherDataManagerが天気データの更新イベントをWeatherViewControllerに通知するよう設定する
+        searchField.delegate = self // UITextFieldの編集イベントをWeatherViewControllerに通知するよう設定する
     }
 
 
 }
  
 //MARK:- TextField extension
-// UITextFieldDelegateメソッドの実装
+//WeatherViewControllerがUITextFieldDelegate準拠し、WeatherViewControllerを拡張する
 extension WeatherViewController: UITextFieldDelegate {
-    
-    // 検索ボタンがクリックされたときの処理
+        //メソッドや関数の前には、funcが必要
+        //@IBAction は、UI要素のアクションとメソッドを接続するために使用されます。
+
+    // 引数 'sender' は、アクションをトリガーした UIButton のインスタンスを指します
         @IBAction func searchBtnClicked(_ sender: UIButton) {
-            // キーボードを閉じる
-            searchField.endEditing(true)    //dismiss keyboard
-            // 検索フィールドのテキストを出力
-            print(searchField.text!)
-            // 天気情報の検索
+            searchField.endEditing(true)    //dismiss keyboard     // キーボードを閉じるために、searchField（テキストフィールド）の編集を終了する
+            print(searchField.text!)     // テキストフィールドの内容をコンソールに出力する
             searchWeather()
         }
-    // 天気情報を検索するメソッド
+    
         func searchWeather(){
-            // テキストフィールドから都市名を取得し、天気情報の取得をリクエスト
+            //searchField.textがnilでない場合、trueになる
             if let cityName = searchField.text{
+                //var weatherManager = WeatherDataManager()
+                //fetchWeather(cityName) は WeatherDataManager クラスのメソッドであり、cityName という引数を受け取って天気データを取得する機能を持っている
                 weatherManager.fetchWeather(cityName)
             }
         }
         
         // when keyboard return clicked
-    // キーボードのリターンキーがクリックされたときの処理
+    //textFieldShouldReturn メソッドは、
         func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-            // キーボードを閉じる
             searchField.endEditing(true)    //dismiss keyboard
-            // 検索フィールドのテキストを出力
             print(searchField.text!)
-            // 天気情報の検索
+            
             searchWeather()
             return true
         }
         
-    // テキストフィールドが非選択になるときの処理
         // when textfield deselected
+        //textFieldShouldEndEditingはUITextFieldDelegate プロトコルに定義されてる
+        //このメソッドはテキストフィールドがリターンキー（Enterキー）を押されたときに呼び出される
         func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
             // by using "textField" (not "searchField") this applied to any textField in this Controller(cuz of delegate = self)
-            // テキストフィールドにテキストが入力されている場合は、編集を終了
+            //textFieldが空でない場合"true"を返す
             if textField.text != "" {
                 return true
-                // テキストフィールドが空の場合は、プレースホルダーを設定して編集を終了しない
             }else{
+            //text.Fieldが空の場合、textFieldに"Type something here"が表示
                 textField.placeholder = "Type something here"
+                //return false で、編集が終了しないようにしている
                 return false            // check if city name is valid
             }
         }
         
         // when textfield stop editing (keyboard dismissed)
-    // テキストフィールドの編集が終了したときの処理
+        //textFieldDidEndEditing(_:) メソッドは UITextFieldDelegate プロトコルに定義されてる
+        //このメソッドは、テキストフィールドが編集を終了したとき（ユーザーがキーボードを閉じる、または他の場所をタップするなど）に呼び出されます。　実行内容がコメントアウトしてるから何も実行してない
         func textFieldDidEndEditing(_ textField: UITextField) {
-            // ここではテキストフィールドの内容をクリアする処理などが可能ですが、コメントアウトされています
-                    // searchField.text = ""   // clear textField
     //        searchField.text = ""   // clear textField
+            
         }
 }
 
 //MARK:- View update extension
-// WeatherManagerDelegateメソッドの実装
 extension WeatherViewController: WeatherManagerDelegate {
     
-    // 天気情報を更新するメソッド
     func updateWeather(weatherModel: WeatherModel){
-        // メインスレッドでUIの更新を行う
         DispatchQueue.main.sync {
-            // ラベルと画像を更新
             temperatureLabel.text = weatherModel.temperatureString
             cityLabel.text = weatherModel.cityName
             self.conditionImageView.image = UIImage(systemName: weatherModel.conditionName)
         }
     }
     
-    // エラーが発生したときの処理
     func failedWithError(error: Error){
         print(error)
     }
 }
 
 // MARK:- CLLocation
-// CLLocationManagerDelegateメソッドの実装
 extension WeatherViewController: CLLocationManagerDelegate {
     
-    // 現在地ボタンがクリックされたときの処理
     @IBAction func locationButtonClicked(_ sender: UIButton) {
         // Get permission
-        // 位置情報の使用許可をリクエスト
         locationManager.requestWhenInUseAuthorization()
-        // 現在地の取得をリクエスト
         locationManager.requestLocation()
     }
     
-    // 位置情報が更新されたときの処理
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // 最後の位置情報を取得
         if let location = locations.last {
-            // 緯度と経度を取得し、天気情報の取得をリクエスト
             let lat = location.coordinate.latitude
             let lon = location.coordinate.longitude
             weatherManager.fetchWeather(lat, lon)
         }
     }
-    // 位置情報の取得に失敗したときの処理
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
